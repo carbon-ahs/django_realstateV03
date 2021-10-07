8648b35dbd237b90dabb1a731b543cfa8bdf2f52
carbon-ahs <shehanuk.ahsan@gmail.com>
2021-10-07 19:47:02 +0600

    paginator done, single page listing going to setup

--- listings/views.py
+++ listings/views.py
@@ -1,17 +1,22 @@
+from django.core import paginator
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger 
 
 from .models import Listing
 
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
     context = {
-        'listings': listings,
+        'listings': paged_listings,
         'key': 'value',
     }
     return render(request, 'listings/listings.html', context)
 
-def listing(request):
+def listing(request, listing_id):
 
     context = {
         'key': 'value',
